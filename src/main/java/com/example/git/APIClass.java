5be5bf3b6c7adf4bf74b50206abb073f02eef945
rahul <dev.rahulprasad@gmail.com>
2021-07-06 13:55:17 +0530

    added api for default path

---
+++ src/main/java/com/example/git/APIClass.java
@@ -0,0 +1,15 @@
+package com.example.git;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class APIClass {
+
+    @GetMapping
+    public String get() {
+        return "hello";
+    }
+}
